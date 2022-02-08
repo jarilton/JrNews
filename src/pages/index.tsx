9904f482bda15e8507600bf51a2d4d4d4d1c0d3c
jarilton <jariltonpereira02@hotmail.com>
2022-02-07 22:36:19 -0300

    Estilizando com SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
-    <h1>Seja todos bem vindos ao Jr News</h1>
+    <h1 className={styles.title}>
+      Seja todos <span>bem vindos</span> ao Jr News
+    </h1>
   )
 }
