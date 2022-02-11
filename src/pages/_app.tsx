48947010c6b3bf5c28fab257d3c36d71a7abf27d
Jarilton <jariltonpereira02@hotmail.com>
2022-02-11 11:10:17 -0300

    Autenticação com NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/global.scss'
 
-import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ 
+  Component, 
+  pageProps: { session, ...pageProps}, 
+}) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
