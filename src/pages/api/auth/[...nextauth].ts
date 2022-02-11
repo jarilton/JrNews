29b2dfc867f64f699e5efaea52c2e35fa70962b7
Jarilton <jariltonpereira02@hotmail.com>
2022-02-11 12:39:58 -0300

    Salvando usuário no banco de dados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,6 @@
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
@@ -6,7 +9,25 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      /* scope: 'read:user' */
     }),
   ],
+
+    callbacks: { 
+      async signIn({ user, account, profile,}) {
+        const { email } = user
+
+        try {
+          await fauna.query(
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            )
+          )
+          return true
+
+        } catch {
+           return false           
+        }
+    }
+  }
 })
