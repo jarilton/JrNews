425d659a17207b145185eafa362129af26317299
jarilton <jariltonpereira02@hotmail.com>
2022-02-13 11:23:57 -0300

    Ouvindo eventos do stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,19 +1,55 @@
-import { NextApiResponse, NextApiRequest } from 'next';
+import { stripe } from './../../services/stripe';
+import { NextApiResponse, NextApiRequest } from 'next'
+import { Readable } from 'stream'
+import Stripe from 'stripe'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    console.log('Evento recebido')
+async function buffer(readable: Readable) {
+    const chucks = []
 
-    res.status(200).json({ ok: true })
+    for await (const chuck of readable) {
+        chucks.push(
+            typeof chuck === "string" ? Buffer.from(chuck) : chuck
+        )   
+    }
+
+    return Buffer.concat(chucks)
+
 }
 
-/* 
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
 
--> instalar o stripe cli
-=> rodar stripe.exe no cmd/cmder
--> stripe login
--> enter
--> stripe listen --forward-to localhost:3000/api/webhooks (caminho da api do webhooks no vscode)
--> testar na aplicação clicando no botão de se inscreve
--> verificar no terminal e no vscode se rodou com sucesso.
+const relevantEvents = new Set ([
+    'checkout.session.completed'
+])
+    
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === "POST") {
+        const buf = await buffer(req)
+        const secret = req.headers['stripe-signature']
 
-*/
+        let event: Stripe.Event
+
+        try {
+            event = stripe.webhooks.constructEvent(buf, secret. process.env.STRIPE_WEBHOOK_SECRET)
+
+        } catch(err) {
+            return res.status(400).send(`Webhook error: ${err.message}`)
+        }
+
+        const { type } = event.type
+
+        if(relevantEvents.has(type)) {
+            console.log('Evento recebido', event)
+        }
+
+        res.json({ received: true })
+
+    } else {
+        res.setHeader('Allow', 'POST')
+        res.status(405).end('Method not allowed') 
+    }
+}
