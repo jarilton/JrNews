4bb66c012d4d4102ceebfc1faaa77a87cd62a14c
Jarilton <jariltonpereira02@hotmail.com>
2022-02-18 17:50:31 -0300

    Navegação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
+import Link from 'next/link'
+
 import { SignInButton } from './SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
-    return(
+    return (
         <>
             <header className={styles.headerContainer}>
                 <div className={styles.headerContent}>
                     <img src="/images/logo.png" alt="Jr News" />
                     <nav>
-                        <a className={styles.active}>Inicio</a>
-                        <a>Postagens</a>
+                        <Link href="/">
+                            <a className={styles.active}>Inicio</a>
+                        </Link>
+                        <Link href="/posts">
+                            <a>Postagens</a>
+                        </Link>
                     </nav>
 
                     <SignInButton />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,21 +1,8 @@
-import * as prismic from '@prismicio/client'
-import fetch from 'node-fetch'
-
 import Head from 'next/head';
 import styles from './styles.module.scss';
-import { RichText } from 'prismic-dom';
 
-type Posts = {
-    slug: string;
-    title: string;
-    excerpt: string;
-    updatedAt: string;
-}
-interface PostsProps {
-    posts: Posts[]
-}
 
-export default function Posts({ posts }: PostsProps) {
+export default function Posts() {
     return (
         <>
             <Head>
@@ -23,55 +10,18 @@ export default function Posts({ posts }: PostsProps) {
             </Head>
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    {posts.map(post => (
-                        <a key={post.slug} href="#">
-                            <time>{post.updatedAt}</time>
-                            <strong>{post.title}</strong>
-                            <p>{post.excerpt}</p>
-                        </a>
-                    ))}
+                    <a href='#'>
+                        <time>01 de março de 1998</time>
+                        <strong>O ano do nascimento do Jamal</strong>
+                        <p>Deus sabe de todas as coisas, confia nele e ele tudo fará! Se tu creres, tu verás a glória de Deus.</p>
+                    </a>
+                    <a href='#'>
+                        <time>10 de junho de 1992</time>
+                        <strong>Copa do mundo</strong>
+                        <p>O senhor é meu pastor e nada me faltará.</p>
+                    </a>
                 </div>
             </main>
         </>
     )
 }
-
-const routes = [
-    {
-        type: 'post',
-        path: '/:uid',
-    },
-]
-
-const repoName = 'your-repo-name'
-const endpoint = prismic.getEndpoint(repoName)
-const client = prismic.createClient(endpoint, { routes, fetch })
-
-const init = async () => {
-    const response = await client.getAllByType('post', {
-        orderings: {
-            field: 'document.first_publication_date',
-            direction: 'desc',
-        },
-        lang: 'pt-BR',
-    })
-    const posts = response.map(post => {
-        return {
-            slug: post.uid,
-            title: RichText.asText(post.data.title),
-            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
-            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
-                day: '2-digit',
-                month: 'long',
-                year: 'numeric'
-            })
-        }
-    })
-    return {
-        props: {
-            posts
-        }
-    }
-}
-
-init()
