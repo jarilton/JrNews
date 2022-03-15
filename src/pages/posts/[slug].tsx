5ce151ff83b1a8cc10191cb3fbd9b4f731f61ec8
jarilton <jariltonpereira02@hotmail.com>
2022-03-15 15:33:14 -0300

    Configurando testing library

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,5 @@
+export default function Post() {
+  return(
+    <h1>Testando...</h1>
+  )
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -10,12 +10,12 @@ export default function Posts() {
             </Head>
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href='#'>
+                    <a href='/post'>
                         <time>01 de março de 1998</time>
                         <strong>O ano do nascimento do Jamal</strong>
                         <p>Deus sabe de todas as coisas, confia nele e ele tudo fará! Se tu creres, tu verás a glória de Deus.</p>
                     </a>
-                    <a href='#'>
+                    <a href=''>
                         <time>10 de junho de 1992</time>
                         <strong>Copa do mundo</strong>
                         <p>O senhor é meu pastor e nada me faltará.</p>
