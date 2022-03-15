c3cb757a8ec8a7a4e6dc7706d72a57b698c5a74a
jarilton <jariltonpereira02@hotmail.com>
2022-03-15 16:50:42 -0300

    Testando o componente "ActiveLink"

---
+++ src/components/ActiveLink/ActiveLink.test.tsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react'
+import { ActiveLink } from '.';
+
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/'
+      }
+    }
+  }
+})
+
+describe('ActiveLink Component', () => {
+  //primeiro jeito de testar
+  test('active link renders corerectly', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText('Home')).toBeInTheDocument()
+  })
+
+  // segundo jeito pra testar
+  it(' adds active class if the link as currently active', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText('Home')).toHaveClass('active');
+  })
+})
+
